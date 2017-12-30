d5132b2cf02ab7f754045839e89ee10cc277707f
Caique <caique.c.s@outlook.com>
2017-12-30 13:21:25 -0300

    added js file initial logic

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 
+var counter = 1;
+
 function reply_click(clicked_id)
 {
-    alert(clicked_id)
+    if(counter == 1){
+        document.getElementById('p1').innerHTML = clicked_id;
+        counter++;
+    }else{
+        document.getElementById('p2').innerHTML = clicked_id;
+        counter = 1;
+    }
 }
